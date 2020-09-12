5bff0ff02399c75191c6f0a6fb147529c7f6154f
felipelopes01 <FelipeLopesSantosoficial@outlook.com>
2020-09-12 18:37:30 -0300

    Estágio 1 pronto

--- calculos.js
+++ calculos.js
@@ -2,11 +2,10 @@ let QualiNo
 let QualiOr 
 let QuantDis 
 let QuantCon 
-let Validar = true
 
 QualiNo = true
 
-let Vet = ["Grau de Escolaridade", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "Ensino Superior", "Ensino Superior" , "Ensino Médio", "Ensino Médio", "Ensino Médio", "muito bom", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "pessimo", "Ensino Fundamental", "Ensino Médio","Ensino Médio", "Ensino Médio"]
+let Vet = ["Grau de Escolaridade", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "Ensino Superior", "Ensino Superior" , "Ensino Médio", "Ensino Médio", "Ensino Médio", "muito bom", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "pessimo", "Ensino Fundamental", "Ensino Médio","Ensino Médio", "Ensino Médio", "Ensino Fundamental"]
 
 let Obj = {
     Titulo: "",
@@ -14,28 +13,32 @@ let Obj = {
 
     }
 }
-
 let Calcular = () =>{
     for(Verifica in Vet){
-
         if (Verifica == 0){
              Obj.Titulo = Vet[Verifica]
-             alert("Novo Título adicionado: ", Vet[Verifica])
+             alert("Novo Título adicionado: " + Vet[Verifica])
         } else{
-            console.log(Obj)
             if(Obj.Itens.hasOwnProperty([Vet[Verifica]])){
                 Obj.Itens[Vet[Verifica]]++
-                alert(Vet[Verifica], " foi Incrementado!")
+                alert(Vet[Verifica] + " foi Incrementado!")
             }else{
                 Obj.Itens[Vet[Verifica]] = 1
-                alert("Novo Item adicionado: ", Vet[Verifica])
+                alert("Novo Item adicionado: " + Vet[Verifica])
             }
         }
     }
     
-     console.log(Obj)
+    console.log(Obj)
+    const secao = document.querySelector('section')
     if (QualiNo){
-       
+        let criadiv = document.createElement('div')
+        criadiv.innerHTML += "Título da pesquisa: " + Obj.Titulo + '<br/>' + '<br/>'
+
+        for(Atual in Obj.Itens){
+            criadiv.innerHTML +=  Atual + " Tendo: "+ Obj.Itens[Atual] + '<br/>'
+        }
+        secao.appendChild(criadiv)
     }else if (QualiOr) {
         
     }else if (QuantDis) {
@@ -44,4 +47,3 @@ let Calcular = () =>{
       
     }
 }
-Calcular()
