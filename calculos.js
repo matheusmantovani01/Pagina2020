f5db38d3f6e220c16a3413c8ef0006a28e2f8dc5
felipelopes01 <FelipeLopesSantosoficial@outlook.com>
2020-09-12 16:06:34 -0300

    Atualizar

--- calculos.js
+++ calculos.js
@@ -3,25 +3,31 @@ let QualiOr = getElementById(QlO)
 let QuantDis = getElementById(QtD)
 let QuantCon = getElementById(QtC)
 
-let Obj = {}
+QualiNo = true
 
-}
+let Vet = ["Grau de Escolaridade", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "Ensino Superior", "Ensino Superior" , "Ensino Médio", "Ensino Médio", "Ensino Médio", "muito bom", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "pessimo", "Ensino Fundamental", "Ensino Médio","Ensino Médio", "Ensino Médio"]
+
+let Obj = {}
 
 let Calcular = () =>{
     if (QualiNo){
-        for(Verifica of Obj){
-
+        for(Verifica of Vet.length){
+           if (Verifica == 0){
+                Obj.titulo = Vet[Verifica]
+           } else{
+               
+           }
         }
     }else if (QualiOr) {
-        for(Verifica of Obj){
+        for(Verifica of Vet.length){
             
         }
     }else if (QuantDis) {
-        for(Verifica of Obj){
+        for(Verifica of Vet.length){
             
         }
     }else {
-        for(Verifica of Obj){
+        for(Verifica of Vet.length){
             
         }
     }
