f81ef8e24f0317f9316bf019cc413d2617863f0f
felipelopes01 <FelipeLopesSantosoficial@outlook.com>
2020-09-12 15:40:29 -0300

    Atualizar

--- calculos.js
+++ calculos.js
@@ -1,24 +1,19 @@
-const LTDA = () => {
-    const readline = require('readline')
-    const fs = require('fs')
-    const readble = fs.createReadStream()
-    readble.line[0]
-
-  
-}
-
 let QualiNo = getElementById(QlN)
 let QualiOr = getElementById(QlO)
 let QuantDis = getElementById(QtD)
 let QuantCon = getElementById(QtC)
 
-if(QuliNo){
+let Obj = {}
 
-}else if(QualiOr){
+    if(QuliNo){
 
-}else if(QuantDis){
-
-}else if(QuantCon){
+    }else if(QualiOr){
+    
+    }else if(QuantDis){
+        
+    }else if(QuantCon){
+    
+    }
 
 }
 
