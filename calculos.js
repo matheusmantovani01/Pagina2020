01e8075979dd5db285504f206a483c3f4a054ca1
felipelopes01 <FelipeLopesSantosoficial@outlook.com>
2020-09-13 13:59:08 -0300

    In

--- calculos.js
+++ calculos.js
@@ -2,8 +2,10 @@ let QualiNo
 let QualiOr 
 let QuantDis 
 let QuantCon 
+let caso
 
-QualiNo = true
+QualiNo = false
+QualiOr = true
 
 let Vet = ["Grau de Escolaridade", "Ensino Superior", "Ensino Médio", "Ensino Fundamental", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "Ensino Superior", "Ensino Superior" , "Ensino Médio", "Ensino Médio", "Ensino Médio", "muito bom", "Ensino Fundamental", "Ensino Médio", "Ensino Superior", "pessimo", "Ensino Fundamental", "Ensino Médio","Ensino Médio", "Ensino Médio", "Ensino Fundamental"]
 
@@ -17,20 +19,20 @@ let Calcular = () =>{
     for(Verifica in Vet){
         if (Verifica == 0){
              Obj.Titulo = Vet[Verifica]
-             alert("Novo Título adicionado: " + Vet[Verifica])
         } else{
             if(Obj.Itens.hasOwnProperty([Vet[Verifica]])){
                 Obj.Itens[Vet[Verifica]]++
-                alert(Vet[Verifica] + " foi Incrementado!")
             }else{
                 Obj.Itens[Vet[Verifica]] = 1
-                alert("Novo Item adicionado: " + Vet[Verifica])
+                
             }
         }
-    }
-    
+    } 
+
     console.log(Obj)
+
     const secao = document.querySelector('section')
+
     if (QualiNo){
         let criadiv = document.createElement('div')
         criadiv.innerHTML += "Título da pesquisa: " + Obj.Titulo + '<br/>' + '<br/>'
@@ -39,8 +41,19 @@ let Calcular = () =>{
             criadiv.innerHTML +=  Atual + " Tendo: "+ Obj.Itens[Atual] + '<br/>'
         }
         secao.appendChild(criadiv)
+
     }else if (QualiOr) {
-        
+        for(Atual in Obj.Itens){
+            Atual.toUpperCase
+            if (Atual == ("Janeiro" || "Fevereiro" || "Março" || "Abril" || "Maio" || "Junho" || "Julho" || "Agosto" || "Setembro" || "Outubro" || "Novembro" || "Dezembro").toUpperCase()){
+                caso = 1
+                break
+            }else if(Atual.indexOf("DOUTORADO" || "MESTRADO" || "PÓS GRADUAÇÃO" || "PÓS-GRADUAÇÃO" || "POS GRADUAÇÃO" || "POS-GRADUAÇÃO" || "MUITO-ALTO" || "MUITO ALTO" || "ALTO" || "SUPERIOR" || "MUITO BOM" || "MUITO-BOM" || "BOM" || "INTERMEDIÁRIO" || "INTERMEDIARIO" || "MEDIO"  || "MEDIANO"  || "RAZOAVEL"  || "FUNDAMENTAL" || "RUIM" || "MUITO-RUIM" || "MUITO RUIM" || "INFANTIL" || "PESSIMO"  || "PÉSSIMO" || "DETESTÁVEL"  || "DETESTAVEL")) {
+                caso = 2
+            }else{
+                caso = 0
+            }
+        }
     }else if (QuantDis) {
         
     }else {
