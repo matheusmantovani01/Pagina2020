8360196d8b68f00ba4e5d1c7383f0c27a0ed648d
felipelopes01 <FelipeLopesSantosoficial@outlook.com>
2020-10-12 16:29:43 -0300

    atualizando

--- calculos.js
+++ calculos.js
@@ -19,29 +19,36 @@ const Popula  =  document.getElementById("Popula")//Popula
 
 var tabela = document.querySelector("tbody")
 
-let tipo
+let tipo // muda a o tipo do grafico
 let media
 let moda
-let modas
+let modas // auxiliar da moda
 let mediana
-let medianas
-let caso = 0
-let it
-let contador = 0
-let guardar
-let guardar2
-let passou = false
-let I = []
-let G = []
-let M = []
-let soma
-let Frs = []
-let Fas = []
-let Fr = []
-let Fa = []
+let medianas // auxiliar da mediana
+let caso = 0 // ainda sem uso
+let contador = 0 // auxiliar
+let guardar // auxiliar
+let guardar2 // auxiliar
+let passou = false // idenficidaor para tarefas que n podem ser repitidas
+let I = [] // valor do item
+let G = [] // nome do item
+let M = [] // media dos itens
+let soma // auxiliar para somar itens
+let Frs = [] // Frequencia Relativa Simples
+let Fas = [] // Frequencia Acumulada Simples
+let Fr = [] // Frequencia Relativa %
+let Fa = [] // Frequencia Acumulada %
 let criaitem = document.createElement('th')
-let desvio
-let cofvaria
+let desvio // Desvio padrão
+let cofvaria // coeficiente de variação
+let percent // porcentagem da medida separatriz calculada
+let posicao // posição em que resultou a medida
+let MedidaSeparitriz // guarda o valor da medida direto em sua porcenagem
+
+let Quartil
+let Quintil
+let Decil
+let Percentil
 
 
 QualiNo = true
@@ -57,12 +64,15 @@ QuantDis = false
 
 let Vet = ["Titulo", 32, 46, 90, 40, 57, 32, 32, 33, 50, 42, 35, 38, 35, 41, 34, 52, 32, 75, 69, 44, 36, 42, 60, 56, 30, 38, 37, 79, 45, 37, 30, 32, 62, 50, 45, 41, 59, 31, 66, 39, 43, 33, 70, 50, 47, 30, 36, 40, 67, 39, 80, 89, 90, 35, 36, 89, 76, 78, 90, 49, 54, 63, 37, 36, 78, 89, 45, 56, 67, 78, 69, 47, 36, 57, 68, 79, 88, 76, 72, 91, 92, 93, 54, 65, 66, 38, 37, 48, 100, 65, 67, 89, 79, 89, 81, 82, 53, 65, 78, 90]
 
+//objeto que guarda os itens
 let Obj = {
     Titulo: "",
     Itens:{
 
     }
 }
+
+// a function adiciona os itens do vetor ao objeto
 let Objeto = () =>{
     for(Verifica in Vet){
         
@@ -76,14 +86,8 @@ let Objeto = () =>{
     }
 } 
 
-let percent
-let posicao
-let MedidaSeparitriz
-let Quartil
-let Quintil
-let Decil
-let Percentil
 
+// define a medida separatiz utilizada (ainda sem uso)
 let Medida = () => {
     if(Quartil){
         MedidaSeparitriz = Quartil
@@ -111,18 +115,22 @@ let Calcular = () =>{
 
     const tabela = document.querySelector('tr')
 
-    if (false){
+    if (QualNominal){
+        // define o tipo do grafico
         tipo = "pie"
 
+        //retira o primeiro item do vetor e trnasforma  em titulo
         Obj.Titulo = Vet.shift()
 
+        //transforma o vet no obj
         Objeto()
 
+        //calculos para poder utlizar as medidas separatrizes
         MedidaSeparitriz = 25
         percent = (MedidaSeparitriz * Vet.length) / 100
         percent = parseInt(percent)
-        contador = 0
 
+        //transforma o obj em vetores separados de itens e quantidade exemplo I[0] corresponde ao valor do item 1 e G[0] corresponde ao titulo do item 1
         contador = 0
         for(Atual in Obj.Itens){
             I[contador] = Obj.Itens[Atual]
@@ -134,12 +142,15 @@ let Calcular = () =>{
 
         soma = Vet.length
         console.log(soma)
-        contador = 0
 
+        contador = 0
             for(item of I){
+                //identifica se ja está na posição que foi calculada para a medida separatriz
                 if (contador == percent){
+                    //salva a posição como texto
                     posicao = G[contador]
                 }
+                //calcula as Freuências
                 Fr[contador] = (item / soma) * 100
                 Frs[contador] = item
                 if (contador == 0){
@@ -152,12 +163,13 @@ let Calcular = () =>{
                 contador ++
             }
 
+            //calcula moda media e meidana
             media = "não tem"
             mediana = parseInt(Vet.length/2)
             medianas = 0
             moda = -99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
             for(moderar in I){
-                medianas = medianas + I[moderar]
+                medianas += I[moderar]
                 if(passou == false){
                     if(medianas >= mediana){
                         mediana = G[moderar]
@@ -174,12 +186,14 @@ let Calcular = () =>{
             console.log(moda)
             console.log(mediana)
             
+            // reseta a variavel "passou"
             passou = false
             
             
 
             
-    }else if (false) {
+    }else if (QualOrdinal) {
+        //neste momento faz o mesmo que a anterior, então leve em conta os mesmos comentarios
         tipo = "pie"
 
         Obj.Titulo = Vet.shift()
@@ -238,26 +252,34 @@ let Calcular = () =>{
             console.log(moda)
             console.log(mediana)
 
-    }else if (false) {
+    }else if (QtDiscreta) {
+        // define o tipo do grafico
         tipo = "bar"
 
+        //retira o primeiro item do vetor e trnasforma  em titulo
         Obj.Titulo = Vet.shift()
 
+        //transforma as variaveis em numerico
         for (let prop in Vet) {
             Vet[prop] = parseFloat(Vet[prop])
         }
 
+        //transforma o vet no obj
         Objeto()
 
+        //calculos para poder utlizar as medidas separatrizes
         MedidaSeparitriz = 25
         percent = (MedidaSeparitriz * Vet.length) / 100
         percent = parseInt(percent)
-        contador = 0
 
+        contador = 0
         for(Atual in Obj.Itens){
+            //identifica se ja está na posição que foi calculada para a medida separatriz
             if (Atual == percent){
+                //salva a posição como texto
                 posicao = Obj.Itens[Atual]
             }
+            //transforma o obj em vetores separados de itens e quantidade exemplo I[0] corresponde ao valor do item 1 e G[0] corresponde ao titulo do item 1 e os ordena
             if (contador == 0) {
                 I[contador] = Obj.Itens[Atual]
                 G[contador] = Atual
@@ -280,7 +302,7 @@ let Calcular = () =>{
         console.log(G)
         console.log(Vet)
 
-        
+        //calcula as Freuências
         soma = Vet.length
         console.log(soma)
         contador = 0
@@ -298,11 +320,11 @@ let Calcular = () =>{
                 contador ++
             }
 
+            //calcula a moda, media e meidana
             media = 0 
             for(mediar of Vet){
                 media += mediar
             }
-
             media = media/Vet.length
             mediana = parseInt(Vet.length/2)
             medianas = 0
@@ -325,6 +347,7 @@ let Calcular = () =>{
             console.log(moda)
             console.log(mediana)
 
+            //calcula o desvio
             desvio = 0
             for(mediar in I){
                 guardar = G[mediar] - media
@@ -336,32 +359,37 @@ let Calcular = () =>{
             desvio = Math.sqrt(desvio)
             console.log(desvio)
 
+            //calcula o coeficiente de variação
             cofvaria = (desvio / media) * 100
             
             console.log(cofvaria)
 
             passou = true
     }else {
-
-        
+        // define o tipo do grafico
         tipo = "bar"
 
+        //retira o primeiro item do vetor e trnasforma  em titulo
         Obj.Titulo = Vet.shift()
 
+        //transforma as variaveis em numerico
             for (let prop in Vet) {
                 Vet[prop] = parseFloat(Vet[prop])
             }
-
+        
+        //ordena o vetor
         Vet.sort(function(a, b) {
         return a - b;
         });
-
+        
+        //caucula a classe
         guardar = Vet.length - 1
         let At = Vet[guardar] - Vet[0] + 1
         guardar =Math.sqrt(Vet.length)
         let clasess = parseInt(guardar)
-        let intervalo
 
+        //calcula o intervalo
+        let intervalo
         while(passou == false){
             if(At % (clasess - 1) == 0){
                 passou = true
@@ -380,6 +408,7 @@ let Calcular = () =>{
             }
         }
 
+        //calculos para poder utlizar as medidas separatrizes
         MedidaSeparitriz = 25
         percent = (MedidaSeparitriz * Vet.length) / 100
         percent = parseInt(percent)
@@ -389,6 +418,8 @@ let Calcular = () =>{
         console.log(At)
         console.log(clasess)
         console.log(intervalo)
+
+        // transforma o Vetor em objetos e calcula a medida separatriz e a media de cada item
         let nome
         guardar = Vet[0]
         let soma = 0
@@ -415,6 +446,7 @@ let Calcular = () =>{
         }
         console.log(Obj)
 
+        //transforma o obj em vetores separados de itens e quantidade exemplo I[0] corresponde ao valor do item 1 e G[0] corresponde ao titulo do item 1        
         contador = 0
         for(Atual in Obj.Itens){
             I[contador] = Obj.Itens[Atual]
@@ -424,7 +456,7 @@ let Calcular = () =>{
         console.log(I)
         console.log(G)
 
-    
+        // Calcula as frquencias
         soma = Vet.length
         console.log(soma)
         contador = 0
@@ -442,6 +474,7 @@ let Calcular = () =>{
                 contador ++
             }
         
+        //calcula a media geral
         contador = 0
         media = 0
         for(mediar of M){
@@ -449,13 +482,13 @@ let Calcular = () =>{
             contador ++
         }
 
+        // define moda media e meidana
         passou = false
         media = media/Vet.length
         console.log(media)
         mediana = 0
         contador = 0
         guardar = 0
-
         for(mediar of I){
             console.log(mediar)
             mediana += mediar
@@ -468,12 +501,9 @@ let Calcular = () =>{
             }
             contador ++
         }
-        
         mediana = media +((((Vet.length / 2) - medianas) / guardar)* intervalo)
         console.log(M)
         console.log(mediana)
-        
-
         contador = 0
         guardar = -99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
         for(mediar of I){
@@ -491,6 +521,7 @@ let Calcular = () =>{
         console.log(Frs)
         console.log(Fas)
 
+        //calcula o coeficiente de variação e desvio padrão
         guardar = 0
         guardar2 = 0
         desvio = 0
@@ -500,47 +531,54 @@ let Calcular = () =>{
             guardar = guardar2 * I[mediar]
             desvio += guardar
         }
-
         desvio = desvio / Vet.length
         desvio = Math.sqrt(desvio)
         console.log(desvio)
-
         cofvaria = (desvio / media)*100
-            
         console.log(cofvaria)
 
         passou = true
     }
 
+    // passa os items gerados pelos possiveis calculos para a tabela
+
+    //cria a linha
     var linha = document.createElement('tr');
+    //cria os campos dentro da linha
     var campo_nome = document.createElement('td');
     var campo_Frs = document.createElement('td');
     var campo_Fr = document.createElement('td');
     var campo_Fas = document.createElement('td');
     var campo_Fa = document.createElement('td');
 
+    //cria um texto
     var texto_nome = document.createTextNode(Obj.Titulo);
     var texto_Frs = document.createTextNode("Frquência Relativa Simples:");
     var texto_Fr = document.createTextNode("Frquência Relativa (%):");
     var texto_Fas = document.createTextNode("Frquência Acumulada Simples:");
     var texto_Fa = document.createTextNode("Frquência Acumulada (%):");
 
+    //adiciona o texto aos campos
     campo_nome.appendChild(texto_nome);
     campo_Frs.appendChild(texto_Frs);
     campo_Fr.appendChild(texto_Fr);
     campo_Fas.appendChild(texto_Fas);
     campo_Fa.appendChild(texto_Fa);
 
+    //adiciona os campos a linha
     linha.appendChild(campo_nome);
     linha.appendChild(campo_Frs);
     linha.appendChild(campo_Fr);
     linha.appendChild(campo_Fas);
     linha.appendChild(campo_Fa);
 
+    //define uma classe css para a linha
     linha.className = "tg"
 
+    //faz com que a tabela receba a nova linha
     tabela.appendChild(linha)
 
+    //(os comentarios acima servem para os demais abaixo)
     for(var Atual = 0; Atual < I.length; Atual++){
         var linha = document.createElement('tr');
         var campo_nome = document.createElement('td');
